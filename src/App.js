ee7d5ad0bb4d7df1ff534352cb585fff5aa4b6c0
Foster Amponsah Asante <fostersnt@gmail.com>
2024-08-11 14:58:05 +0000

    Added interface

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import './App.css';
 import Home from './views/home.tsx';
 
 function App() {
+  const data = {
+    name: 'Calculator',
+    cost: 150.00
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Home/>
+        <Home {...data}/>
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 
-const Home: React.FC = () => {
+interface HomeProps{
+    name: String,
+    cost: Number
+}
+
+const Home: React.FC<HomeProps> = ({name, cost}) => {
     return (
         <div>
             <h1>Home</h1>
             <p>
-                {/* Edit <code>src/App.js</code> and save to reload. */}
-                My first react app in town
+                The name of the app is {name} and the cost is Ghs{cost.toString()} 
             </p>
         </div>
     )
