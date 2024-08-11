114116293061280cb7c8cdf2f9910fe9c699ccd2
Foster Amponsah Asante <fostersnt@gmail.com>
2024-08-11 08:08:12 +0000

    Created first tsx file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './views/home.tsx';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {/* Edit <code>src/App.js</code> and save to reload. */}
-          My first react app
-        </p>
+        <Home/>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/views/home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home: React.FC = () => {
+    return (
+        <div>
+            <h1>Home</h1>
+            <p>
+                {/* Edit <code>src/App.js</code> and save to reload. */}
+                My first react app in town
+            </p>
+        </div>
+    )
+}
+
+export default Home;
